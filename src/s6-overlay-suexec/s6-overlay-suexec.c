51a5cf468f44832c53c6c1ca145e11c74d984112
Laurent Bercot <ska-skaware@skarnet.org>
2022-02-04 19:00:02 +0000

    First attempt at supporting --userns=host
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/s6-overlay-suexec/s6-overlay-suexec.c
+++ src/s6-overlay-suexec/s6-overlay-suexec.c
@@ -1,5 +1,10 @@
 /* ISC license. */
 
+/* for setresuid/setresgid */
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include <unistd.h>
 #include <sys/wait.h>
 #include <pwd.h>
@@ -23,22 +28,23 @@
 int main (int argc, char const **argv, char const *const *envp)
 {
   pid_t pid ;
-  uid_t uid ;
-  gid_t gid ;
+  uid_t euid = geteuid(), uid = getuid() ;
+  gid_t gid = getgid() ;
   int argc1, wstat ;
   size_t len = env_len(envp) ;
-  size_t m = 12 ;
+  size_t m = 0 ;
   unsigned int n = 0 ;
   char fmt[MAXLEN] = "GIDLIST\0GID=" ;
-
   PROG = "s6-overlay-suexec" ;
 
   if (!--argc) strerr_dieusage(100, USAGE) ;
-  if (getpid() != 1) strerr_dief1x(100, "can only run as pid 1") ;  
-  if (geteuid()) strerr_dief1x(100, "insufficient privileges (is the suid bit set?)") ;
-
-  uid = getuid() ;
-  gid = getgid() ;
+  if (getpid() != 1) strerr_dief1x(100, "can only run as pid 1") ;
+  if (euid)
+  {
+    strerr_warnwu1x("gain root privileges (is the suid bit set?)") ;
+    if (setresgid(gid, gid, gid) == -1) strerr_diefu1sys(111, "setresgid") ;
+    if (setresuid(uid, uid, uid) == -1) strerr_diefu1sys(111, "setresuid") ;
+  }
 
   argc1 = el_semicolon(++argv) ;
   if (argc1 >= argc) strerr_dief1x(100, "unterminated block") ;
@@ -46,14 +52,21 @@ int main (int argc, char const **argv, char const *const *envp)
   
   {
     char const *v[len + 4] ;
-    m += gid_fmt(fmt + m, gid) ;
-    memcpy(fmt + m, "\0UID=", 5) ; m += 5 ;
-    m += uid_fmt(fmt + m, uid) ;
-    fmt[m++] = 0 ;
+    if (!euid)
+    {
+      memcpy(fmt + m, "GIDLIST\0GID=", 12) ; m += 12 ;
+      m += gid_fmt(fmt + m, gid) ; fmt[m++] = 0 ;
+      memcpy(fmt + m, "\0UID=", 5) ; m += 5 ;
+      m += uid_fmt(fmt + m, uid) ;
+      fmt[m++] = 0 ;
+    }
     if (!env_mergen(v, len + 4, envp, len, fmt, m, 3))
       strerr_diefu1sys(111, "merge environment") ;
-    if (gid) if (setgid(0) == -1) strerr_diefu4sys(111, "set", "g", "id to ", "root") ;
-    if (uid) if (setuid(0) == -1) strerr_diefu4sys(111, "set", "u", "id to ", "root") ;
+    if (!euid)
+    {
+      if (gid) if (setgid(0) == -1) strerr_diefu4sys(111, "set", "g", "id to ", "root") ;
+      if (uid) if (setuid(0) == -1) strerr_diefu4sys(111, "set", "u", "id to ", "root") ;
+    }
     pid = el_spawn0(argv[0], argv, v) ;
     if (!pid) strerr_diefu2sys(111, "spawn ", argv[0]) ;
   }
@@ -69,47 +82,49 @@ int main (int argc, char const **argv, char const *const *envp)
   {
     char fmt[UINT_FMT] ;
     fmt[uint_fmt(fmt, WEXITSTATUS(wstat))] = 0 ;
-    strerr_dief2x(wait_estatus(wstat), "child failed wth exit code ", fmt) ;
+    strerr_dief2x(wait_estatus(wstat), "child failed with exit code ", fmt) ;
   }
-
-  if (gid) if (setgid(gid) == -1) strerr_diefu4sys(111, "set", "g", "id to ", "user") ;
-  if (uid) if (setuid(uid) == -1) strerr_diefu4sys(111, "set", "u", "id to ", "user") ;
 
   m = 0 ;
-  if (gid)
+  if (!euid)
   {
-    struct group *gr = getgrgid(gid) ;
-    memcpy(fmt + m, "CONTAINER_GID=", 14) ; m += 14 ;
-    m += gid_fmt(fmt + m, gid) ;
-    fmt[m++] = 0 ; n++ ;
-    if (gr)
+    if (gid) if (setgid(gid) == -1) strerr_diefu4sys(111, "set", "g", "id to ", "user") ;
+    if (uid) if (setuid(uid) == -1) strerr_diefu4sys(111, "set", "u", "id to ", "user") ;
+    if (gid)
     {
-      size_t rlen = strlen(gr->gr_name)  + 1 ;
-      memcpy(fmt + m, "CONTAINER_GROUP=", 16) ; m += 16 ;
-      if (rlen > MAXGROUPLEN)
+      struct group *gr = getgrgid(gid) ;
+      memcpy(fmt + m, "CONTAINER_GID=", 14) ; m += 14 ;
+      m += gid_fmt(fmt + m, gid) ;
+      fmt[m++] = 0 ; n++ ;
+      if (gr)
       {
-        errno = ENAMETOOLONG ;
-        strerr_diefu4sys(111, "fill CONTAINER_", "GROUP", " environment variable with value ", gr->gr_name) ;
+        size_t rlen = strlen(gr->gr_name)  + 1 ;
+        memcpy(fmt + m, "CONTAINER_GROUP=", 16) ; m += 16 ;
+        if (rlen > MAXGROUPLEN)
+        {
+          errno = ENAMETOOLONG ;
+          strerr_diefu4sys(111, "fill CONTAINER_", "GROUP", " environment variable with value ", gr->gr_name) ;
+        }
+        memcpy(fmt + m, gr->gr_name, rlen) ; m += rlen ; n++ ;
       }
-      memcpy(fmt + m, gr->gr_name, rlen) ; m += rlen ; n++ ;
     }
-  }
-  if (uid)
-  {
-    struct passwd *pw = getpwuid(uid) ;
-    memcpy(fmt + m, "CONTAINER_UID=", 14) ; m += 14 ;
-    m += uid_fmt(fmt + m, uid) ;
-    fmt[m++] = 0 ; n++ ;
-    if (pw)
+    if (uid)
     {
-      size_t rlen = strlen(pw->pw_name)  + 1 ;
-      memcpy(fmt + m, "CONTAINER_USER=", 15) ; m += 15 ;
-      if (rlen > MAXUSERLEN)
+      struct passwd *pw = getpwuid(uid) ;
+      memcpy(fmt + m, "CONTAINER_UID=", 14) ; m += 14 ;
+      m += uid_fmt(fmt + m, uid) ;
+      fmt[m++] = 0 ; n++ ;
+      if (pw)
       {
-        errno = ENAMETOOLONG ;
-        strerr_diefu4sys(111, "fill CONTAINER_", "USER", " environment variable with value ", pw->pw_name) ;
+        size_t rlen = strlen(pw->pw_name)  + 1 ;
+        memcpy(fmt + m, "CONTAINER_USER=", 15) ; m += 15 ;
+        if (rlen > MAXUSERLEN)
+        {
+          errno = ENAMETOOLONG ;
+          strerr_diefu4sys(111, "fill CONTAINER_", "USER", " environment variable with value ", pw->pw_name) ;
+        }
+        memcpy(fmt + m, pw->pw_name, rlen) ; m += rlen ; n++ ;
       }
-      memcpy(fmt + m, pw->pw_name, rlen) ; m += rlen ; n++ ;
     }
   }
 
